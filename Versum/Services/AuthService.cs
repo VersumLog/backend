c10d025868c61293a30c527356d6bc2e4e9cfb7c
Tachyon64 <83309505+Tachyon64@users.noreply.github.com>
2026-04-16 16:15:34 +0300

    Implemented Password Recovery for users

--- Versum/Services/AuthService.cs
+++ Versum/Services/AuthService.cs
@@ -14,6 +14,9 @@ namespace Versum.Services
         // string? Field = what field has error (or null if everything is ok)
 
         Task<(bool success, string tokenOrError, string userGmail, string username)> LoginAsync(LoginDto dto);
+
+        Task<(bool success, string? error)> ForgotPasswordAsync(ForgotPasswordDto dto);
+        Task<(bool success, string? error)> ResetPasswordAsync(ResetPasswordDto dto);
     }
 
     public class AuthService : IAuthService {
@@ -98,7 +101,60 @@ namespace Versum.Services
             return (true, jwtToken, user.Gmail, user.Username);
         }
 
+        public async Task<(bool success, string? error)> ForgotPasswordAsync(ForgotPasswordDto dto)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(u => u.Username == dto.Email);
+            if (user == null)
+            {
+                return (true, null); //Returning true even if email doesn't exit for account safety
+            }
+            string ResetToken = Guid.NewGuid().ToString(); // Generating a new GUID token
+            user.PasswordResetToken = ResetToken;
+            user.ResetTokenExpires = DateTime.UtcNow.AddHours(1); // Token is valid for one hour after creation
 
+            try
+            {
+                await _db.SaveChangesAsync();
+
+                //Email interaction not implemented yet
+                //await _emailService.SendResetPasswordEmailAsync(user.Username, resetToken);
+
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, "Помилка на сервері при обробці запиту");
+            }
+        }
+        public async Task<(bool success, string? error)> ResetPasswordAsync(ResetPasswordDto dto)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(u => u.PasswordResetToken == dto.Token);
+            if (user == null)
+            {
+                return (false, "Недійсний токен.");
+            }
+            if (user.ResetTokenExpires < DateTime.UtcNow)
+            {
+                return (false, "Термін дії токена вичерпано. Запитуйте відновлення знову.");
+            }
+            try
+            {
+                user.PasswordResetToken = null;
+                user.ResetTokenExpires = null;
+                string passwordHash = BCrypt.Net.BCrypt.HashPassword(dto.NewPassword);
+
+                await _db.SaveChangesAsync();
+                return (true, null);
+            }
+            catch(DbUpdateException ex)
+            {
+                return (false, "Сталася помилка при зверненні до бази даних.");
+            }
+            catch(Exception ex)
+            {
+                return (false, "Сталася непередбачувана помилка на сервері.");
+            }
+        }
 
 
 
