bba0a4abd3bb463ed5f5e6a2fcbb5fefa531cd2b
Tachyon64 <83309505+Tachyon64@users.noreply.github.com>
2026-04-18 18:37:53 +0300

    Reapply "Reapply "Implemented Password Recovery for users""
    This reverts commit 15d6ad7f4985b8b36bc8c99fda13dac25bd93561.

--- Versum/Controllers/AuthController.cs
+++ Versum/Controllers/AuthController.cs
@@ -61,6 +61,33 @@ namespace Versum.Controllers
             return Ok(new { token = resultMessage, message = "Вхід успішний" });
         }
 
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotReset([FromBody] ForgotPasswordDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);// checks validation attributes from LoginDto -> Smth wrong -> returns error 400
+            var (success, error) = await _authService.ForgotPasswordAsync(dto);
+            if (!success)
+            {
+                return BadRequest(new { message = error });
+            }
+
+            return Ok(new { message = "Лист надіслано" });
+
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetReset([FromBody] ResetPasswordDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);// checks validation attributes from LoginDto -> Smth wrong -> returns error 400
+            var (success, error) = await _authService.ResetPasswordAsync(dto);
+            if (!success)
+            {
+                return BadRequest(new { message = error });
+            }
+            return Ok(new { message = "Пароль успішно змінено" });
+        }
 
         // Allow to see added users in the table(only for dev to try it out): shall be deleted or changed.
         [HttpGet("users")]
---
+++ Versum/Dtos/ForgotPasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Versum.Dtos
+{
+    public class ForgotPasswordDto
+    {
+        [Required(ErrorMessage = "Введіть email")]
+        [MaxLength(50, ErrorMessage = "Поле не може містити більше 50-ти символів")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
