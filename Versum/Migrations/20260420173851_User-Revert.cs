9cd6a14335c51158d25047e5b55b024d75113236
Tachyon64 <83309505+Tachyon64@users.noreply.github.com>
2026-04-22 22:33:17 +0300

    Feature/password reset email template
    * [REFACTOR] Improved password reset email method
    
    * Email template now shows Username
    
    * Fixed passwords requiring exactly 8 characters when resetting a password instead of intended 8-20
    
    * Removed a redundant commented method from AuthService and email template copyright line

---
+++ Versum/Migrations/20260420173851_User-Revert.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Versum.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRevert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
