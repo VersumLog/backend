439302c79e57c1fccbb750a5e4ccb84a7e3bc160
Tachyon64 <83309505+Tachyon64@users.noreply.github.com>
2026-04-22 22:33:17 +0300

    Feature/password reset email template
    * [REFACTOR] Improved password reset email method
    
    * Email template now shows Username
    
    * Fixed passwords requiring exactly 8 characters when resetting a password instead of intended 8-20
    
    * Removed a redundant commented method from AuthService and email template copyright line

--- Versum/Services/EmailService.cs
+++ Versum/Services/EmailService.cs
@@ -1,14 +1,30 @@
-﻿using MailKit.Net.Smtp;
+﻿using Microsoft.AspNetCore.Hosting;
+using MailKit.Net.Smtp;
 using MimeKit;
 using Microsoft.Extensions.Options;
 
 public class EmailService : IEmailService
 {
     private readonly IConfiguration _config;
+    private readonly IWebHostEnvironment _env;
 
-    public EmailService(IConfiguration config)
+    public EmailService(IConfiguration config, IWebHostEnvironment env)
     {
         _config = config;
+        _env = env;
+    }
+
+    public async Task SendResetCodeEmailAsync(string toEmail, string userName, string code)
+    {
+        string templatePath = Path.Combine(_env.ContentRootPath, "Templates", "ForgotPasswordTemplate.html");
+
+        string htmlContent = await File.ReadAllTextAsync(templatePath);
+
+        htmlContent = htmlContent
+            .Replace("{UserName}", userName)
+            .Replace("{RESET_TOKEN}", code);
+
+        await SendEmailAsync(toEmail, "Ваш код для зміни пароля", htmlContent);
     }
 
     public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
@@ -21,7 +37,7 @@ public class EmailService : IEmailService
         emailMessage.To.Add(new MailboxAddress("", toEmail));
         emailMessage.Subject = subject;
 
-        // 2. Email body
+        // Email body
         emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
         {
             Text = htmlMessage
