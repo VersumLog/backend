4382f6933fd5ef4a60c07d693d4cbadcf19e318d
Denis <gumenyuk.dn@gmail.com>
2026-04-30 10:16:42 +0300

    Feature/delete account (#16)
    * add delete and anonymize account endpoint

---
+++ Versum/Dtos/DeleteAccountDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Versum.Dtos
+{
+    public class DeleteAccountDto
+    {
+        [Required(ErrorMessage = "Введіть свій пароль")]
+        [StringLength(20, MinimumLength = 8,
+             ErrorMessage = "Пароль має містити від 8 до 20 символів")]
+        [RegularExpression(@"^\S+$",
+             ErrorMessage = "Пароль не може містити пробіли")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
+}
