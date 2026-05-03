73738384dd944c209650ccad9356373bb54f2c6c
NotAsasha <notyasasha@gmail.com>
2026-05-03 18:08:35 +0300

    [Fix] Removed Redundant Migrations

--- Versum/Migrations/20260503150257_Posts Table Rework.cs
+++ Versum/Migrations/20260503150257_Posts Table Rework.cs
@@ -5,7 +5,7 @@
 namespace Versum.Migrations
 {
     /// <inheritdoc />
-    public partial class UserRevert : Migration
+    public partial class PostsTableRework : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
