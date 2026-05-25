cbb32933df605955ecb8d7ed04463c0432304b6f
NotAsasha <notyasasha@gmail.com>
2026-05-25 18:52:13 +0300

    [FIX] Megration conflicts fix

---
+++ Versum/Migrations/20260525154409_Merge 2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Versum.Migrations
+{
+    /// <inheritdoc />
+    public partial class Merge2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
